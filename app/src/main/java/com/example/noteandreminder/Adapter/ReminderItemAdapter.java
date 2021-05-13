182f81fdf492ae4a37711243cee131c8b364c24e
DauMoe <lehuuyhoang1111999@gmail.com>
2021-05-13 17:30:06 +0700

    create reminder dialog

--- app/src/main/java/com/example/noteandreminder/Adapter/ReminderItemAdapter.java
+++ app/src/main/java/com/example/noteandreminder/Adapter/ReminderItemAdapter.java
@@ -1,13 +1,18 @@
 package com.example.noteandreminder.Adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.noteandreminder.Module.Reminder;
@@ -16,12 +21,7 @@ import com.example.noteandreminder.R;
 import java.util.List;
 
 public class ReminderItemAdapter extends RecyclerView.Adapter<ReminderItemAdapter.ReminderItemViewHolder> {
-    private Context context;
     private List<Reminder> data;
-
-//    public ReminderItemAdapter(Context context) {
-//        this.context = context;
-//    }
 
     public void setData(List<Reminder> x) {
         this.data = x;
@@ -43,6 +43,22 @@ public class ReminderItemAdapter extends RecyclerView.Adapter<ReminderItemAdapte
         holder.reminder_detail_desc.setText(item.getReminder_desc());
         holder.reminder_detail_time.setText(String.valueOf(item.getRemind_time()));
         holder.reminder_detail_completed.setChecked(item.isReminder_completed());
+        if (item.isReminder_completed()) {
+            holder.remider_detail_title.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            holder.reminder_detail_desc.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+        holder.reminder_detail_completed.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    holder.remider_detail_title.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                    holder.reminder_detail_desc.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+                } else {
+                    holder.remider_detail_title.setPaintFlags(Paint.LINEAR_TEXT_FLAG);
+                    holder.reminder_detail_desc.setPaintFlags(Paint.LINEAR_TEXT_FLAG);
+                }
+            }
+        });
     }
 
     @Override
@@ -54,8 +70,11 @@ public class ReminderItemAdapter extends RecyclerView.Adapter<ReminderItemAdapte
     public class ReminderItemViewHolder extends RecyclerView.ViewHolder {
         private TextView remider_detail_title, reminder_detail_desc, reminder_detail_time;
         private CheckBox reminder_detail_completed;
+        private ConstraintLayout reminder_detail;
+
         public ReminderItemViewHolder(@NonNull View itemView) {
             super(itemView);
+            reminder_detail = itemView.findViewById(R.id.reminder_detail);
             remider_detail_title = itemView.findViewById(R.id.reminder_detail_title);
             reminder_detail_desc = itemView.findViewById(R.id.reminder_detail_desc);
             reminder_detail_completed = itemView.findViewById(R.id.reminder_detail_checkbox);
